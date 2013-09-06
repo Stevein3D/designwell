21a72e02162a8dce69a92cf20319a6c9d60cd491
Steve Sikoryak <stevein3d@gmail.com>
2013-09-06 09:48:36 -0400

    Initial link

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Designwell::Application.config.secret_key_base = '3e05ada1ea1eb41a3ca9bfeb93ec1c0b4f16c0f44eb8b4bf1a2cc746b21dfefecd1cac61682c4519c499b7d9ec8af750f9facbc6276c22fadefb78fc1ed2182d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Designwell::Application.config.session_store :cookie_store, key: '_designwell_session'
