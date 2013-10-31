516168f06da2261b83c4a9ba4d030de8c1cc0d7a
Steve Sikoryak <stevein3d@gmail.com>
2013-10-31 14:16:13 -0400

    Remove redundant js reference and redesign of the index menu system.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 
-	$( "h1" ).mouseenter(function() {
-			var color = $( this ).css( "background-color" );
-		$( ".bar" ).css( 'background-color','' + color + '');
-			});
+/* ==========================================================================
+   Adjust height of sidebar with page resize
+   ========================================================================== */
 
 	$(function(){
 
@@ -15,6 +14,33 @@ $(document).ready(function(){
        $vertbar.height(height);
     }).trigger('resize'); //on page load
 
-});
+  });
+
+/* ==========================================================================
+   Menu and submenu animations
+   ========================================================================== */
+
+  $('.nav-button').click(function(){
+    var menuSelect = $(this).siblings('.sub-menu');
+    $('.sub-menu').not(menuSelect).slideUp(300,'swing');
+    $(menuSelect).slideToggle(300,'swing');
+
+
+    // $('.sub-menu').slideUp(300,'swing');
+    
+  });
+  
+  // $('.category').mouseleave(function(){
+  //   $(this).children('.sub-menu').delay(300).slideUp(300,'swing');
+  // });
+
+  $('h1').mouseenter(function(){
+      var color = $( this ).css( "background-color" );
+    $( ".bar" ).css( 'background-color','' + color + '');
+      });
+
+  
+
 
 });
+
