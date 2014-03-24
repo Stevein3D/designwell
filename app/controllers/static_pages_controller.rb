b8c53d15b646f853134f617444da6f4bbe80d668
Steve Sikoryak <stevein3d@gmail.com>
2014-03-24 00:17:30 -0400

    Link Flickr to the respective categories

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,20 @@
 class StaticPagesController < ApplicationController
+	require 'flickraw'
+
   def index
+  	
+  	FlickRaw.api_key="7fa0cd18055434599246311abac71dc3"
+  	FlickRaw.shared_secret="cd47d71ba69e9219"
+
   	@user = Vimeo::Simple::User.info("designwell")
     @title = Vimeo::Simple::Channel.videos("682861")
     @animation = Vimeo::Simple::Channel.videos("682882")
     @montage = Vimeo::Simple::Channel.videos("682871")
+    @collateral = flickr.photosets.getPhotos(:photoset_id => "72157641487232383")
+    @poster = flickr.photosets.getPhotos(:photoset_id => "72157635482932841")
+
   end
+
+
   
 end
